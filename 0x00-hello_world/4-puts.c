43c7cefbb0e9833d641c90e494dfa945d5db7158
abdeladimch <choukri.abdeladim@gmail.com>
2023-07-13 10:58:25 -0700

    updated to Betty style documentation

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main is the entry point of the program
- * return result: 0 (success!)
+ * main - is the entry point of the program
+ * Return: 0 (success!)
  **/
 int main(void)
 {
