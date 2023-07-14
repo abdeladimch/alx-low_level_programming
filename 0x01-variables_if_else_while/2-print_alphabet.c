924356f8890bbac4914537424cb06747c4a4d87b
abdeladimch <choukri.abdeladim@gmail.com>
2023-07-14 13:31:10 -0700

    added 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point of the program
+ * Return: 0
+**/
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
