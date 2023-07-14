fcf8f3cce663711b6ed673a26c5fcef1c4d56457
abdeladimch <choukri.abdeladim@gmail.com>
2023-07-14 13:35:42 -0700

    added 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - the netry point of the program
+ * Return: 0
+**/
+
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
