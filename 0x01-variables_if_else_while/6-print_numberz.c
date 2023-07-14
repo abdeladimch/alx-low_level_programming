ca2e2b649a3e0e8fbb3c215cbd379ccb0e0d4a13
abdeladimch <choukri.abdeladim@gmail.com>
2023-07-14 13:36:58 -0700

    added 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point of the program
+ * Return: 0
+**/
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
